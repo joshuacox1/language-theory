a6d72700e51469a2a7c5171a2a13b01335591f25
Joshua Cox <joshuascox@proton.me>
2025-11-02 12:00:36 +0000

    stuff

---
+++ src/blah.rs
@@ -0,0 +1,68 @@
+
+// // use cfg::{ContextFreeGrammar, Variable, Terminal, VarOrTerm,
+// // zv,zt,xv,xt};
+
+// // // mod cfg;
+// // mod regexp;
+
+// use std::io;
+
+// fn main() -> io::Result<()> {
+//     // use contextfree::genregexp::*;
+
+//     let mut buffer = String::new();
+//     loop {
+//         io::stdin().read_line(&mut buffer)?;
+
+//         match contextfree::to_min_dfa_html(&buffer) {
+//             Ok(s) => println!("{s}"),
+//             Err(_) => println!("Parsing error :("),
+//         }
+
+//         buffer.clear();
+//     }
+// }
+
+
+// // fn main() -> io::Result<()> {
+// //     use contextfree::regexp::*;
+// //     let mut buffer = String::new();
+
+// //     loop {
+// //         io::stdin().read_line(&mut buffer)?;
+
+// //         match StateMachine::new(&buffer) {
+// //             Ok(canon_min_dfa) => println!("\n{}", canon_min_dfa.to_graphviz()),
+// //             Err(e) => println!("Parse fail: {e}"),
+// //         }
+
+// //         buffer.clear();
+// //     }
+// // }
+
+// // fn cfg_test_main() {
+// //     let mut test_grammar = ContextFreeGrammar::new(
+// //         [
+// //             (zv('S'), vec![xv('S')]),
+// //             (zv('S'), vec![xt('0')]),
+// //             (zv('S'), vec![xv('A')]),
+// //             (zv('A'), vec![xv('A'), xv('B')]),
+// //             (zv('B'), vec![xt('1')]),
+// //         ].into_iter().collect::<HashSet<_>>(),
+// //         zv('S'),
+// //     );
+// //     println!("{test_grammar:?}");
+
+// //     println!("Culling...");
+// //     let (nongenerative, unreachable) = test_grammar.cull_useless();
+// //     println!("Variables {nongenerative:?} were non-generative");
+// //     println!("Variables {unreachable:?} were unreachable afer removal of non-generative items");
+
+// //     // for r in test_grammar
+// //     println!("{test_grammar:?}");
+// // }
+
+// // S -> 0 | A
+// // A -> S
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,20 @@
 mod genregexp;
 
 use crate::genregexp::GenRegex;
+use wasm_bindgen::prelude::*;
 
 /// Given a generalised regular expression in postfix notation,
 /// returns an HTML presentation of the canonical minimal DFA
 /// representating the same language as the expression.
 /// Returns an error if the input was an invalid expression.
-pub fn to_min_dfa_html(postfix_str: &str) -> Result<String, ()> {
+#[wasm_bindgen]
+pub fn to_min_dfa_html(postfix_str: &str) -> String {
     match GenRegex::from_postfix_str(postfix_str) {
-        None => Err(()),
+        None => String::new(),
         Some(r) => {
             let d = r.to_canon_min_dfa();
             let s = d.show(false);
-            Ok(s)
+            s
         }
     }
 }
-
-
-// pub mod regexp;
-
-
-// use wasm_bindgen::prelude::*;
-
-// #[wasm_bindgen]
-// extern "C" {
-//     pub fn alert(s: &str);
-// }
-
-// #[wasm_bindgen]
-// pub fn greet(name: &str) {
-//     alert(&format!("Hello, {}!", name));
-// }
